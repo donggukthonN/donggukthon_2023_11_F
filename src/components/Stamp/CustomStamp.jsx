507ccd9c0b98b0afb239966a084e83d1963de484
g2Min <jimin9320@gmail.com>
2023-12-19 04:32:46 +0900

    feat: 커스텀 우표 컴포넌트

---
+++ src/components/Stamp/CustomStamp.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+import frame from '../../assets/icon/stamp_frame.svg';
+import question from '../../assets/icon/no_image.svg';
+import giftStamp from '../../assets/icon/gift_icon.svg'; // api연결 후 삼항연산자 처리
+
+const CustomStamp = ({userId}) => {
+  return(
+    <Container>
+      <Frame>
+        <Image src={question}>
+        </Image>
+      </Frame>
+    </Container>
+  );
+};
+
+export default CustomStamp;
+
+
+const Container = styled.div`
+  display: flex; 
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 730px;
+  border: 1px solid black;
+  background-color: #FFF6D4;
+`;
+
+const Frame = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid black; 
+  width: 206px; 
+  height: 206px;
+  background-image: url(${frame});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat; /*반복 방지*/
+  // background-color: white;
+`;
+
+const Image = styled.img`
+  width: 90px;
+  height: 130px;
+`;
