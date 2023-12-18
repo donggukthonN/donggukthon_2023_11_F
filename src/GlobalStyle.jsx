3171031ba156f652f8397b1cffec09e6694248a9
g2Min <jimin9320@gmail.com>
2023-12-19 04:32:06 +0900

    style: 전역 스타일관리

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStye = createGlobalStyle`
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
+
+  /* 커스텀 글씨체 표시*/
+  @font-face {
+    font-family: "YClover-Bold";
+    src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_231029@1.1/YClover-Bold.woff2")
+      format("woff2");
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  /*태블릿 및 모바일 화면을 위한 스타일링*/
+  @media (max-width: 768px) {
+
+  }
+
+  /*모바일 화면을 위한 스타일링*/
+  @media (max-width: 480px) {
+
+  }
+
+  body {
+    /* 커스텀 글씨체 표시*/
+    @font-face {
+      font-family: "YClover-Bold";
+      src: url("https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_231029@1.1/YClover-Bold.woff2")
+        format("woff2");
+      font-weight: 700;
+      font-style: normal;
+    }
+    margin: 0;
+    padding: 0;
+    display: flex;
+    justify-content: center; /* 가로 중앙 정렬 */
+    align-items: center; /* 세로 중앙 정렬 */
+    line-height: 1.5;
+  }
+
+  // .container{
+  //   max-width: 400px;
+  //   width: 100%;
+  //   max-height: 749px;
+  //   height: 100%;
+  // }
+`;
+
+export default GlobalStye;
