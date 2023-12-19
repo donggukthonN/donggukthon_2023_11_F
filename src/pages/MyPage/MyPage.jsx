6110d165310485491326bfe9571d317a123cbc06
g2Min <jimin9320@gmail.com>
2023-12-19 20:32:18 +0900

    feat: mypage 버튼 컴포넌트 전환

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,19 +1,27 @@
 import MenuBar from "../../components/common/MenuBar";
 import { 
   Container,
-  ListContainer
+  ListContainer,
+  LeftBtn,
+  RightBtn
 } from './styled';
 import { useRecoilState } from "recoil";
-import { storageState } from "../../stores/storage-state";
+import { stampState } from "../../stores/stamp-state";
 
 const MyPage = () => {
-  const [page, setPage] = useRecoilState(storageState);
+  const [stamp, setStamp] = useRecoilState(stampState);
+  const handleStamp = () => {
+    setStamp(true);
+  };
+  const handleSeal = () => {
+    setStamp(false);
+  };
   return(
     <Container style={{backgroundColor: '#FFF6D4'}}>
-      <MenuBar/>
-      <ListContainer>
-
-      </ListContainer>
+      <div style={{display: 'flex', flexDirection: 'row', marginLeft: 25, marginTop: 36}}> 
+        <LeftBtn onClick={handleStamp} $stamp={stamp}>우표</LeftBtn>
+        <RightBtn onClick={handleSeal} $stamp={stamp}>씰</RightBtn>
+      </div>
     </Container>
   );
 };
--- src/stores/storage-state.js
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-
-export const storageState = atom({
-  key: 'storageState',
-  default: 'stamp'
-})
