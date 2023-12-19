5c894739ff4739d1f8749430a48fc777d5066d13
g2Min <jimin9320@gmail.com>
2023-12-19 23:01:16 +0900

    feat: 우표 페이지 구현

---
+++ src/components/MyPage/Carousel.jsx
@@ -0,0 +1,107 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useState, useEffect } from 'react';
+import CustomStamp from './CustomStamp';
+import styled from 'styled-components';
+import { CarouselContainer } from './styled';
+
+const CustomCarousel = () => {
+  const [currentIndex, setCurrentIndex] = useState();
+  const [components, setComponents] = useState([]);
+  const handleChange = (index) => {
+    setCurrentIndex(index);
+  }
+  const ImageArray = () => {
+    let images = [];
+    for(let i=0; i<10; i++){
+      images.push(<CustomStamp/>);
+    };
+    console.log(images);
+    setComponents(images);
+  };
+  const renderSlides = components.map((item, index) => (
+    <div key={index}>
+      {item}
+    </div>
+  ));
+
+  return(
+    <CarouselContainer>
+      <Carousel
+        showArrows={true}
+        renderArrowPrev={(onClickHandler, hasPrev) => (
+          <CustomPrevArrow onClickHandler={onClickHandler} hasPrev={hasPrev} hasNext={true} label="Prev" />
+        )}
+        renderArrowNext={(onClickHandler, hasNext) => (
+          <CustomNextArrow onClickHandler={onClickHandler} hasPrev={true} hasNext={hasNext} label="Next" />
+        )}
+        onChange={handleChange}
+        selectedItem={ImageArray[currentIndex]}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        infiniteLoop={true}
+        >
+          {Array(10).fill().map((item,index) => (
+            <div key={index} style={{display: 'flex', justifyContent: 'center'}}>
+              <CustomStamp/>
+            </div>
+          ))}
+        </Carousel>
+        {/* <CustomStamp/> */}
+    </CarouselContainer>
+  );
+};
+
+export default CustomCarousel;
+
+const CustomArrowsContainer = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const CustomArrow = styled.div`
+  z-index: 2;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 35px;
+  height: 35px;
+  background-color: #992D2D;
+  top: 50%;
+  border-radius: 50%;
+  color: white;
+  font-size: 24px;
+  line-height: 1;
+  cursor: pointer;
+  &:hover {
+    cursor: pointer;
+    background-color: #7c3aed;
+  }
+`;
+
+const CustomPrevArrow = ({ onClickHandler, hasPrev }) => {
+  if (!hasPrev) return null;
+  return (
+    <CustomArrowsContainer style={{ left: '0' }}>
+      <CustomArrow onClick={onClickHandler} className="arrow-prev">
+        &lt;
+      </CustomArrow>
+    </CustomArrowsContainer>
+  );
+};
+
+const CustomNextArrow = ({ onClickHandler, hasNext, label }) => {
+  if (!hasNext) return null;
+  return (
+    <CustomArrowsContainer style={{ right: '0' }}>
+      <CustomArrow onClick={onClickHandler} className="arrow-next">
+        &gt;
+      </CustomArrow>
+    </CustomArrowsContainer>
+  );
+};
--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,12 +1,20 @@
-import MenuBar from "../../components/common/MenuBar";
 import { 
   Container,
-  ListContainer,
   LeftBtn,
-  RightBtn
+  RightBtn,
+  OrderDiv,
+  OrderImg,
+  OrderText,
+  MainContainer
 } from './styled';
 import { useRecoilState } from "recoil";
 import { stampState } from "../../stores/stamp-state";
+import santaHat from '../../assets/icon/santa_hat.svg';
+import MyStamp from "../../components/MyPage/MyStamp";
+import MySeal from "../../components/MyPage/MySeal";
+
+//api로 number 불러오기
+const number = 0;
 
 const MyPage = () => {
   const [stamp, setStamp] = useRecoilState(stampState);
@@ -17,11 +25,20 @@ const MyPage = () => {
     setStamp(false);
   };
   return(
-    <Container style={{backgroundColor: '#FFF6D4'}}>
+    <Container style={{backgroundColor: '#FFF6D4', maxHeight: 693}}>
       <div style={{display: 'flex', flexDirection: 'row', marginLeft: 25, marginTop: 36}}> 
         <LeftBtn onClick={handleStamp} $stamp={stamp}>우표</LeftBtn>
         <RightBtn onClick={handleSeal} $stamp={stamp}>씰</RightBtn>
+        <OrderDiv>
+          <OrderImg src={santaHat}/>
+          <OrderText>{number} 장 제작했어요 !</OrderText>
+        </OrderDiv>
       </div>
+      <MainContainer>
+        {stamp ? 
+          <MyStamp/>
+        : <MySeal/>}
+      </MainContainer>
     </Container>
   );
 };
