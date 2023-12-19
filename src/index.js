3439c4066022a39292e387509c491a7a4e52f83e
g2Min <jimin9320@gmail.com>
2023-12-20 05:41:27 +0900

    style: 눈내리는 화면

--- src/index.js
+++ src/index.js
@@ -14,14 +14,17 @@ import StampNamingPage from "./pages/StampNaming/StampNamingPage";
 import StampMakingPage from "./pages/StampMaking/StampMakingPage";
 import StampDetailPage from "./pages/StampDetail/StampDetailPage";
 import MySealListPage from "./pages/MySealList/MySealListPage";
-import Example from "./pages/Start/Example";
 import NicknamePage from "./pages/Start/NicknamePage";
 import MyPage from "./pages/MyPage/MyPage";
 import LoadingPage from "./pages/Loading/LoadingPage";
 import { Layout } from "./routes/Layout/Layout";
 import SealMaking from "./pages/SealMaking/SealMaking";
-import SealForU from "./components/SealMaking/SealForU";
 import StampListPage from "./pages/StampList/StampListPage";
+import SealComplete from "./pages/SealMaking/SealComplete";
+import SealAdd from "./pages/SealMaking/SealAdd";
+import SealForStamp from "./pages/StampList/SealForStamp";
+import SealCheck from "./pages/SealMaking/SealCheck";
+import OrderPage from "./pages/Order/OrderPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -34,15 +37,20 @@ root.render(
           <Route path="/signin" element={<NicknamePage />} />
           <Route path="/mypage" element={<Layout />} >
             <Route index path="/mypage" element={<MyPage/>} />
+            <Route path="/mypage/sealmaking" element={<SealMaking/>}/>
+            <Route path="/mypage/sealadd" element={<SealAdd/>} />
+            <Route path="/mypage/stampList" element={<StampListPage />} />
+            <Route path="/mypage/sealList" element={<SealListPage />} />
           </Route>
-          <Route path="/sealmaking" element={<SealMaking/>}/>
-          <Route path="/example" element={<SealForU/>} />
+
+          <Route path="/sealcomplete" element={<SealComplete/>}/>
+          <Route path="/sealcheck" element={<SealCheck/>} />
+          <Route path="/order" element={<OrderPage/>} />
 
           <Route path="/mySealList" element={<MySealListPage />} />
-          <Route path="/sealList" element={<SealListPage />} />
+          {/* <Route path="/sealList" element={<SealListPage />} /> */}
           <Route path="/sealDetail" element={<SealDetailPage />} />
-          <Route path="/example" element={<Example />} />
-          <Route path="/stampList" element={<StampListPage />} />
+          {/* <Route path="/stampList" element={<StampListPage />} /> */}
           <Route path="/stampDetail" element={<StampDetailPage />} />
           <Route path="/invitation" element={<InvitationPage />} />
           <Route path="/stampMaking/:id" element={<StampMakingPage />} />
