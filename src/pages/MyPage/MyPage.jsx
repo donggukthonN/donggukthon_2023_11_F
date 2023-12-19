a7581b37999740fe3603d326c431c94c8e5b7599
g2Min <jimin9320@gmail.com>
2023-12-19 16:11:49 +0900

    feat: 버튼클릭 별로 컴포넌트 전환

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,6 +1,17 @@
+import MenuBar from "../../components/common/MenuBar";
+import { storageState } from "../../stores/storage-state";
+import { 
+  Container
+} from './styled';
+import { useRecoilState } from "recoil";
+
 const MyPage = () => {
+  const [page, setPage] = useRecoilState(storageState);
   return(
-    <></>
+    <Container style={{backgroundColor: '#FFF6D4'}}>
+      <MenuBar/>
+      
+    </Container>
   );
 };
 
